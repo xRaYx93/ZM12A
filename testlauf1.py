ab067339babadb8b5d6af97d7703b1c192e79580
xRaYx93 <raimundkoch@hotmail.de>
2024-07-04 14:23:51 +0200

    Create testlauf1.py

---
+++ testlauf1.py
@@ -0,0 +1,8 @@
+print("hallo, ich bin die erste erstellte datei,\n",
+    "und wurde in vsc geschrieben und erstellt :)\n",
+    "eine dritte Zeile kriegen wir auch noch rein :D\n",
+    "\n",
+    "\n",
+    "\n",
+    "nach 3 Leerzeilen gehts mit Zeile 7 weiter :P\n",
+    "ENDE DER DURCHSAGE")
